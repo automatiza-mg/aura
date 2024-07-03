45fa7e19f340c165a326e72b94f725b502e6d388
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-03 19:32:46 -0300

    Try to join documented schema with described
    Not working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from frictionless import formats, describe, Package
+from frictionless import formats, describe, Package, Schema
+from pathlib import Path
 from sqlalchemy import MetaData
 from models import engine
 import os
@@ -9,11 +10,21 @@ metadata = MetaData()
 metadata.reflect(engine)
 table_names = list(metadata.tables.keys())
 table_names.reverse()
-package = Package(name='teste')
+# import ipdb; ipdb.set_trace(context=10)
+
+dataset_path = Path(__file__).parent / 'dataset'
+datapackage_yaml = dataset_path / 'datapackage.yaml'
+package = Package(datapackage_yaml)
+
+import ipdb; ipdb.set_trace(context=10)
 
 for name in table_names:
+    schema_path = dataset_path / 'schemas' / f'{name}.yaml'
+    schema_yaml = Schema(schema_path)
     control = formats.SqlControl(table=name, basepath='data')
     schema = describe(path=path, name =name, control=control, type="resource")
+    for field in schema.field_names:
+        schema.get_field(field)
     package.add_resource(schema)
     print(schema.to_yaml())
 
