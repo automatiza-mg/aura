a02d5a3fb99d0534206fcb99d6ba077455beba7e
Raiane Cardoso <raianecardoso.rede@gmail.com>
2024-07-19 14:46:18 -0300

    Add tabela sistema aos models

--- models/models.py
+++ models/models.py
@@ -3,3 +3,4 @@ from .pedido_imersao_model import PedidoImersao
 from .projeto_estrategico_model import ProjetoEstrategico
 from .projeto_model import Projeto
 from .robo_model import Robo
+from .sistema_model import Sistema
