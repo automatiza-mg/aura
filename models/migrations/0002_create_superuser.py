46afaabcafbb6f022cadbff72aa3d178170e53db
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-30 20:40:25 -0300

    Cria seed superuser

---
+++ models/migrations/0002_create_superuser.py
@@ -0,0 +1,30 @@
+# your_app/migrations/000X_create_superuser.py
+
+import os
+from dotenv import load_dotenv
+from dotenv import load_dotenv
+from django.db import migrations
+from django.contrib.auth import get_user_model
+
+def create_superuser(apps, schema_editor):
+    User = get_user_model()
+    load_dotenv()
+    username = os.environ.get('ADMIN-USERNAME')
+    email = os.environ.get('ADMIN-EMAIL')
+    password = os.environ.get('ADMIN-PWD')
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password,
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('models', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
