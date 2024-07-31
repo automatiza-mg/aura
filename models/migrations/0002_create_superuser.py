24eab12c80db608582d89d0e8963c3d3ff353a15
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-07-30 21:30:28 -0300

    Try to fix migration error
    Error to migrate and create superuser
    hard code user info
    See https://docs.render.com/configure-environment-variables#python
    See #11

--- models/migrations/0002_create_superuser.py
+++ models/migrations/0002_create_superuser.py
@@ -8,9 +8,9 @@ from django.contrib.auth import get_user_model
 def create_superuser(apps, schema_editor):
     User = get_user_model()
     # load_dotenv()
-    username = os.environ.get('ADMIN_USERNAME')
-    email = os.environ.get('ADMIN_EMAIL')
-    password = os.environ.get('ADMIN_PWD')
+    username = 'admin' #os.environ.get('ADMIN_USERNAME')
+    email = 'admin@example.com' # os.environ.get('ADMIN_EMAIL')
+    password = '123456789SenhaForte'  # os.environ.get('ADMIN_PWD')
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(
             username=username,
