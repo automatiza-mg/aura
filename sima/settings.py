771f8abf8ed10389283cbea2952d25f53fae1885
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-08-01 17:03:42 -0300

    Add postgress db
    See #11
    See https://github.com/automatiza-mg/sima/issues/11#issuecomment-2263543205

--- sima/settings.py
+++ sima/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
-ENV = os.environ.get('ENV')
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '')]
 CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGINS', '')]
@@ -79,13 +78,24 @@ WSGI_APPLICATION = 'sima.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('PG_DB_NAME'),
+            'USER':  os.environ.get('PG_DB_USER'),
+            'PASSWORD':  os.environ.get('PG_DB_PASSWORD'),
+            'HOST':  os.environ.get('PG_DB_HOST'),
+            'PORT':  os.environ.get('PG_DB_PORT'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
