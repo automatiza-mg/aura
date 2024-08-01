673953c05912210fc7bfc2bdcf7ab532d4efe64d
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-08-01 13:12:41 -0300

    Melhora setting.py
    Incluir load_dotenv()
    Melhora chamada variáveis ambiente
    See #11

--- sima/settings.py
+++ sima/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG')
+ENV = os.environ.get('ENV')
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost']
+if ENV == 'dev':
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = ['.onrender.com',]
 
 
 # Application definition
